c59fc9d5f29be51e8ecfc9309b47e050fc9c61e1
shivamwankhade45 <92088219+shivamwankhade45@users.noreply.github.com>
2025-07-22 00:02:23 +0530

    Add files via upload

---
+++ Bus Inventory - Copy/businventory/src/main/java/com/shivam/springboot/businventory/BusinventoryApplication.java
@@ -0,0 +1,13 @@
+package com.shivam.springboot.businventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinventoryApplication.class, args);
+	}
+
+}
